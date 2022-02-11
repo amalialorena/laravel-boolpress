08772d3a2bbfedb0482fbc830fc7b82deb217169
Amalia <amalialorenaferariu@gmail.com>
2022-02-11 09:23:56 +0100

    post edit layout

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -1,46 +1,43 @@
 @extends('layout.main-layout')
 
 @section('content')
-@auth
-    
+    @auth
+        <form class="post-form" action="{{ route('post.update', $post->id) }}" method="POST">
+            @method('POST')
+            @csrf
 
-    <form class="post-form" action="{{ route('post.update', $post -> id) }}" method="POST">
-        @method('POST')
-        @csrf
+            <label for="title"></label>
+            <input type="text" placeholder="title" name="title" value="{{ $post->title }}">
+            <label for="text"></label>
+            <textarea name="text" id="" cols="30" rows="10">{{ $post->text }}</textarea>
 
-        <label for="title"></label>
-        <input type="text" placeholder="title" name="title" value="{{$post -> title}}">
-        <label for="text"></label>
-        <textarea name="text" id="" cols="30" rows="10">{{$post -> text}}</textarea>
-        <select name="category" id="category">
-            @foreach ($categories as $category)
-                <option name="category" value="{{ $category->id }}" 
-                    @if ($category -> id == $post -> category -> id)
+            <select name="category" id="category">
+                @foreach ($categories as $category)
+                    <option name="category" value="{{ $category->id }}" @if ($category->id == $post->category->id)
                         selected
-                    @endif
-                    > {{ $category->name }} </option>
-            @endforeach
-        </select>
-
-        <div>
-            <h4>Tags:</h4>
-            @foreach ($tags as $tag)
-
-            <input type="checkbox"  name="tags[]" value="{{ $tag -> id}}"
-
-                @foreach ($post -> tags as $postTag)
-                
-                    @if ($tag -> id == $postTag -> id)
-                        checked
-                    @endif
-
+                @endif
+                > {{ $category->name }} </option>
                 @endforeach
-            
-            > {{ $tag -> name}} <br>
-            @endforeach
-            <input class="btn btn-primary" type="submit" value="UPDATE">
-        </div>
+            </select>
+            <br>
+            <br>
+            <h4>Tags:</h4>
+            <div class="tags">
 
-    </form>
+                @foreach ($tags as $tag)
+                    <span class="tag">
+
+                        <input type="checkbox" id="tag-{{ $tag->id }}" name="tag-{{ $tag->id }}" value="{{ $tag->id }}" 
+                            @foreach ($post->tags as $postTag)
+                                @if ($tag->id == $postTag->id) checked @endif
+                            @endforeach
+                        >
+                        <label for="tag-{{ $tag->id }}">{{ $tag->name }}</label>
+                        <br>
+                </span>
+                @endforeach
+            </div>
+            <input class="btn btn-primary" type="submit" value="UPDATE">
+        </form>
     @endauth
 @endsection
